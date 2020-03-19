b3e4b4c1d6ca1c7fe1c1103fcc7451d9404a0ee1
isratkhan14 <khanisrat24@gmail.com>
2020-03-19 15:37:26 -0400

    Sel Excer-2 completed one

--- src/main/java/com/practiceExc/Selenium_exer2.java
+++ src/main/java/com/practiceExc/Selenium_exer2.java
@@ -31,13 +31,13 @@ public class Selenium_exer2 {
 	    WebElement model  = dr.findElement(By.name("Model"));
 	    selectFromDropDown(model,"Scooter"); // model
 	    
-	    dr.findElement(By.xpath("//input[@name='Cylinder Capacity']")).sendKeys("200ccm");
-	    dr.findElement(By.xpath("//input[@name='[kW]']")).sendKeys("1500kW");
+	    dr.findElement(By.xpath("//input[@name='Cylinder Capacity']")).sendKeys("200");
+	    dr.findElement(By.xpath("//input[@name='[kW]']")).sendKeys("1500");
 	    dr.findElement(By.name("Date of Manufacture")).sendKeys("02/05/2020");
 	    
 	    WebElement numberOfseat  = dr.findElement(By.name("Number of Seats"));
 	    selectFromDropDown(numberOfseat,"2"); // number of seat
-	    dr.findElement(By.xpath("//label[text()='Yes']")).click(); //radio button
+	   // dr.findElement(By.xpath("//label[text()='Yes']")).click(); //radio button
 	    
 	    WebElement numberOfseat2  = dr.findElement(By.name("Number of Seats Motorcycle"));
 	    selectFromDropDown(numberOfseat2,"3"); // number of seat dropdown 2
@@ -46,7 +46,7 @@ public class Selenium_exer2 {
 	    selectFromDropDown(fuelType,"Diesel"); // Fuel type
 	    
 	    dr.findElement(By.name("Payload")).sendKeys("700"); //payload
-	    dr.findElement(By.name("Total Weight")).sendKeys("2800"); //Total weight
+	    dr.findElement(By.name("Total Weight")).sendKeys("1500"); //Total weight
 	    dr.findElement(By.name("List Price")).sendKeys("70000"); //List Price
 	    dr.findElement(By.name("License Plate Number")).sendKeys("LV500");; //License Plate number
 	    dr.findElement(By.name("Annual Mileage")).sendKeys("30000"); //Annual Mileage
@@ -60,14 +60,14 @@ public class Selenium_exer2 {
 	    
 	    ///////////////////////////////////////////2nd page
 	    
-	    dr.findElement(By.name("First Name")).sendKeys("Aaaa"); //First name
-	    dr.findElement(By.name("Last Name")).sendKeys("Bbbb"); //Last Name
+	    dr.findElement(By.name("First Name")).sendKeys("Ababa"); //First name
+	    dr.findElement(By.name("Last Name")).sendKeys("Abbb"); //Last Name
 	    dr.findElement(By.name("Date of Birth")).sendKeys("09/12/1983"); //Date of birth
 	    
 	    WebElement country  = dr.findElement(By.name("Country")); //country
-	    selectFromDropDown(country,"Austria"); 
+	    selectFromDropDown(country,"Bangladesh"); 
 	    
-	    dr.findElement(By.name("Zip Code")).sendKeys("06654"); //Zip Code
+	    dr.findElement(By.name("Zip Code")).sendKeys("1250"); //Zip Code
 	    
 	    WebElement occupation  = dr.findElement(By.name("Occupation")); //occupation 
 	    selectFromDropDown(occupation,"Public Official");
@@ -82,7 +82,7 @@ public class Selenium_exer2 {
 	    
 	    ////////////////////////////////////////////3rd page
 	    
-	    dr.findElement(By.name("Start Date")).sendKeys("08/08/2020"); //start date
+	    dr.findElement(By.name("Start Date")).sendKeys("08/08/2021"); //start date
 	    
 	    WebElement insurancesum  = dr.findElement(By.name("Insurance Sum")); //Insurance sum
 	    selectFromDropDown(insurancesum,"5000000"); 
@@ -105,6 +105,94 @@ public class Selenium_exer2 {
 	    System.out.println("Third Page title: "+pageTitle3);
 	    Assert.assertTrue(pageTitle3.contains("Choose Price Option"));
 	    
+	    //////////////////////////////////////////////////////////////////////////
+	    
+	    //Reading and printing  the table  
+	    
+	    //Column One
+	    
+	  //*[@id="priceTable"]/tbody/tr[1]/td[1]
+	  //*[@id="priceTable"]/tbody/tr[2]/td[1]
+	  
+	    
+	    String firstPart_Xpath = "//*[@id=\"priceTable\"]/tbody/tr[";
+	    String lastPart_Xpath = "]/td[1]";
+	    for(int i = 1; i<=4; i++) {
+	    	String actualXpath = firstPart_Xpath+i+lastPart_Xpath;
+	    	WebElement el = dr.findElement(By.xpath(actualXpath));
+	    	System.out.println(el.getText());
+	    	
+	    }
+	    
+	  //*[@id="selectsilver_price"]
+	  //*[@id="priceTable"]/tbody/tr[2]/td[2]
+	  
+	    
+	    //Column two
+	    
+	   WebElement rc2 = dr.findElement(By.xpath("//*[@id=\"selectsilver_price\"]"));
+	   String row1Col2 =rc2.getText();
+	   System.out.println("First row of first column: "+row1Col2);
+	   
+	   String lastPart_Xpath_Col2 = "]/td[2]"; 
+	    
+	   for(int i = 2; i<=4; i++) {
+	    	String actualXpath = firstPart_Xpath+i+lastPart_Xpath_Col2;
+	    	WebElement el = dr.findElement(By.xpath(actualXpath));
+	    	System.out.println(el.getText());
+	    	
+	    }
+	   
+	   //Column three
+	   
+	   System.out.println("     Data from the Third column:    ");
+	   WebElement rc3 = dr.findElement(By.xpath("//*[@id=\"selectgold_price\"]"));
+	   String row1Col3 =rc3.getText();
+	   System.out.println(row1Col3);
+	   
+	   String lastPart_Xpath_Col3 = "]/td[3]"; 
+	    
+	   for(int i = 2; i<=4; i++) {
+	    	String actualXpath = firstPart_Xpath+i+lastPart_Xpath_Col3;
+	    	WebElement el = dr.findElement(By.xpath(actualXpath));
+	    	System.out.println(el.getText());
+	    	
+	    }
+	   
+	   //Column Four
+	   
+	   System.out.println("     Data from the 4th column:    ");
+	   WebElement rc4 = dr.findElement(By.xpath("//*[@id=\"selectplatinum_price\"]"));
+	   String row1Col4 =rc4.getText();
+	   System.out.println(row1Col4);
+	   
+	   String lastPart_Xpath_Col4 = "]/td[4]"; 
+	    
+	   for(int i = 2; i<=4; i++) {
+	    	String actualXpath = firstPart_Xpath+i+lastPart_Xpath_Col4;
+	    	WebElement el = dr.findElement(By.xpath(actualXpath));
+	    	System.out.println(el.getText());
+	    	
+	    }
+	   
+     //Column Five
+	   
+	   System.out.println("     Data from the 5th column:    ");
+	   WebElement rc5 = dr.findElement(By.xpath("//*[@id=\"selectultimate_price\"]"));
+	   String row1Col5 =rc5.getText();
+	   System.out.println(row1Col5);
+	   
+	   String lastPart_Xpath_Col5 = "]/td[5]"; 
+	    
+	   for(int i = 2; i<=4; i++) {
+	    	String actualXpath = firstPart_Xpath+i+lastPart_Xpath_Col5;
+	    	WebElement el = dr.findElement(By.xpath(actualXpath));
+	    	System.out.println(el.getText());
+	    	
+	    	
+	    	dr.findElement(By.xpath("//*[@id=\"priceTable\"]/tfoot/tr/th[2]/label[3]/span")).click(); // Radio button for Platinum option
+	    	dr.findElement(By.xpath("//button[@id='nextsendquote']")).click();
+	    }
 }
 	
 	public  void  selectFromDropDown(WebElement element, String value) {
